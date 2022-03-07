17a516feeb3378434ef1eebaf0f98a64a6854b1a
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-03-07 16:17:12 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import tweepy
 
-twitter_oauth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-twitter_oauth.set_access_token("TOKEN", "SECRET")
+twitter_oauth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+twitter_oauth.set_access_token(TOKEN, SECRET)
 api = tweepy.API(twitter_oauth, wait_on_rate_limit=True)
 
 tweets = tweepy.Cursor(api.search_tweets, q="(alfajor OR alfajores) -filter:replies", result_type='recent').items(100)
