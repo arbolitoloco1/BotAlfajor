89614070d53fae16050c6426b51bc346355f1656
ArbolitoLoco <santikzk1406@gmail.com>
2022-03-07 16:08:51 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import tweepy
+
+twitter_oauth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+twitter_oauth.set_access_token("TOKEN", "SECRET")
+api = tweepy.API(twitter_oauth, wait_on_rate_limit=True)
+
+tweets = tweepy.Cursor(api.search_tweets, q="(alfajor OR alfajores) -filter:replies", result_type='recent').items(100)
+
+for tweet in tweets:
+    try:
+        api.retweet(id=tweet.id)
+        print("Retwiteado!")
+    except tweepy.TweepyException as e:
+        print(e)
+        continue
